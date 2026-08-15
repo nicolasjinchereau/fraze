5ff2802981519f853663462828f01764a9a33e80
Nicolas Jinchereau <nicolas.jinchereau@gmail.com>
2026-08-15 15:22:42 -0400

    -fix third_party lib build script to include MIR

--- third_party/build-all.cmd
+++ third_party/build-all.cmd
@@ -6,15 +6,21 @@ call "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevC
 cd assimp\projects\windows
 msbuild assimp.sln /p:Configuration=Debug /t:Clean;Rebuild
 msbuild assimp.sln /p:Configuration=Release /t:Clean;Rebuild
+cd ..\..\..
 
-cd ..\..\..\libpng\projects\windows
+cd libpng\projects\windows
 msbuild libpng.sln /p:Configuration=Debug /t:Clean;Rebuild
 msbuild libpng.sln /p:Configuration=Release /t:Clean;Rebuild
+cd ..\..\..
 
-cd ..\..\..\zlib\projects\windows
+cd mir\projects
+msbuild MIR.sln /p:Configuration=Debug /t:Clean;Rebuild
+msbuild MIR.sln /p:Configuration=Release /t:Clean;Rebuild
+cd ..\..
+
+cd zlib\projects\windows
 msbuild zlib.sln /p:Configuration=Debug /t:Clean;Rebuild
 msbuild zlib.sln /p:Configuration=Release /t:Clean;Rebuild
-
 cd ..\..\..
 
 echo done.
